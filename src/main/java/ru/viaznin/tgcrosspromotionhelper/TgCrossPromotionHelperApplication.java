15f9192fda5891688fdbb7cbd6f6665321437d5b
Ilya Viaznin <viaznin123@gmail.com>
2021-01-24 03:04:12 +0500

    Load native library at startup.

--- src/main/java/ru/viaznin/tgcrosspromotionhelper/TgCrossPromotionHelperApplication.java
+++ src/main/java/ru/viaznin/tgcrosspromotionhelper/TgCrossPromotionHelperApplication.java
@@ -1,13 +1,22 @@
 package ru.viaznin.tgcrosspromotionhelper;
 
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 @SpringBootApplication
 public class TgCrossPromotionHelperApplication {
 
     public static void main(String[] args) {
+        loadTelegramNativeLibrary();
         SpringApplication.run(TgCrossPromotionHelperApplication.class, args);
     }
 
+    @SneakyThrows
+    private static void loadTelegramNativeLibrary() {
+        var lib = new File(System.mapLibraryName("tdjni"));
+        System.load(lib.getAbsolutePath());
+    }
 }
