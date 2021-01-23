39ae7da08d782fdbb5323e8d669cd24ecc3abd48
Ilya Viaznin <viaznin123@gmail.com>
2021-01-23 15:42:59 +0500

    Init.

---
+++ src/main/java/ru/viaznin/tgcrosspromotionhelper/TgCrossPromotionHelperApplication.java
@@ -0,0 +1,13 @@
+package ru.viaznin.tgcrosspromotionhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgCrossPromotionHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgCrossPromotionHelperApplication.class, args);
+    }
+
+}
