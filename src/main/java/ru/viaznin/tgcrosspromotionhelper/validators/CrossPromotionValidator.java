80c120d85ddcf7eca3e3fa038aaa31c6f4b7eff8
Ilya Viaznin <viaznin123@gmail.com>
2021-12-10 19:06:23 +0500

    Edit invitation link validation.

--- src/main/java/ru/viaznin/tgcrosspromotionhelper/validators/CrossPromotionValidator.java
+++ src/main/java/ru/viaznin/tgcrosspromotionhelper/validators/CrossPromotionValidator.java
@@ -14,7 +14,7 @@ public class CrossPromotionValidator extends AbstractValidator<CrossPromotion> {
     @Override
     public void rules() {
         ruleFor(CrossPromotion::getInviteLink)
-                .must(stringContains("https://t.me/joinchat/"))
+                .must(stringContains("https://t.me/"))
                 .when(not(stringEmptyOrNull()))
                 .withMessage("Incorrect telegram invite link!");
     }
